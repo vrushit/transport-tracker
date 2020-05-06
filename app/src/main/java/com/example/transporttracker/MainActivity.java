913a108757d3f402f3a69f027afca4782f0b6ef6
vrushit <bharuchavrushit@ymail.com>
2020-05-06 11:42:41 +0530

    initial commit

---
+++ app/src/main/java/com/example/transporttracker/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.transporttracker;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.Auth;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnTrack;
+
+    private final static int LOGIN_PERMISSION=1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnTrack = (Button)findViewById(R.id.btnTrack);
+        btnTrack.setOnClickListener(new View.OnClickListener() {
+
+            List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().setAllowNewAccounts(true).build());
+
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(providers).build(), LOGIN_PERMISSION
+                );
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
